378dbb72b1f45728bf308ef336c7b1df61a50367
Matthew Wagerfield <matthew@wagerfield.com>
2016-11-30 06:30:53 +0000

    Changed addDoc to addDocs

--- bin/docs.js
+++ bin/docs.js
@@ -1,25 +1,25 @@
-function addDoc(url, postfix) {
+function addDocs(url, postfix) {
   return { url, postfix: postfix || '' }
 }
 
 exports.docs = {
-  'eslint': addDoc('http://eslint.org/docs/rules/'),
-  'angular': addDoc('https://github.com/Gillespie59/eslint-plugin-angular/blob/master/docs/', '.md'),
-  'ava': addDoc('https://github.com/avajs/eslint-plugin-ava/blob/master/docs/rules/', '.md'),
-  'backbone': addDoc('https://github.com/ilyavolodin/eslint-plugin-backbone/blob/master/docs/rules/', '.md'),
-  'flowtype': addDoc('https://github.com/gajus/eslint-plugin-flowtype#'),
-  'import': addDoc('https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/', '.md'),
-  'jasmine': addDoc('https://github.com/tlvince/eslint-plugin-jasmine/blob/master/docs/rules/', '.md'),
-  'jsdoc': addDoc('https://github.com/gajus/eslint-plugin-jsdoc#'),
-  'jsx-a11y': addDoc('https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/', '.md'),
-  'lodash': addDoc('https://github.com/wix/eslint-plugin-lodash/blob/master/docs/rules/', '.md'),
-  'lodash-fp': addDoc('https://github.com/jfmengels/eslint-plugin-lodash-fp/blob/master/docs/rules/', '.md'),
-  'meteor': addDoc('https://github.com/dferber90/eslint-plugin-meteor/blob/master/docs/rules/', '.md'),
-  'mocha': addDoc('https://github.com/lo1tuma/eslint-plugin-mocha/blob/master/docs/rules/', '.md'),
-  'mongodb': addDoc('https://github.com/nfroidure/eslint-plugin-mongodb#'),
-  'react': addDoc('https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/', '.md'),
-  'react-native': addDoc('https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/', '.md'),
-  'requirejs': addDoc('https://github.com/cvisco/eslint-plugin-requirejs/blob/master/docs/rules/', '.md')
+  'eslint': addDocs('http://eslint.org/docs/rules/'),
+  'angular': addDocs('https://github.com/Gillespie59/eslint-plugin-angular/blob/master/docs/', '.md'),
+  'ava': addDocs('https://github.com/avajs/eslint-plugin-ava/blob/master/docs/rules/', '.md'),
+  'backbone': addDocs('https://github.com/ilyavolodin/eslint-plugin-backbone/blob/master/docs/rules/', '.md'),
+  'flowtype': addDocs('https://github.com/gajus/eslint-plugin-flowtype#'),
+  'import': addDocs('https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/', '.md'),
+  'jasmine': addDocs('https://github.com/tlvince/eslint-plugin-jasmine/blob/master/docs/rules/', '.md'),
+  'jsdoc': addDocs('https://github.com/gajus/eslint-plugin-jsdoc#'),
+  'jsx-a11y': addDocs('https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/', '.md'),
+  'lodash': addDocs('https://github.com/wix/eslint-plugin-lodash/blob/master/docs/rules/', '.md'),
+  'lodash-fp': addDocs('https://github.com/jfmengels/eslint-plugin-lodash-fp/blob/master/docs/rules/', '.md'),
+  'meteor': addDocs('https://github.com/dferber90/eslint-plugin-meteor/blob/master/docs/rules/', '.md'),
+  'mocha': addDocs('https://github.com/lo1tuma/eslint-plugin-mocha/blob/master/docs/rules/', '.md'),
+  'mongodb': addDocs('https://github.com/nfroidure/eslint-plugin-mongodb#'),
+  'react': addDocs('https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/', '.md'),
+  'react-native': addDocs('https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/', '.md'),
+  'requirejs': addDocs('https://github.com/cvisco/eslint-plugin-requirejs/blob/master/docs/rules/', '.md')
 }
 
 exports.getRuleDocsUrl = (rule) => {
